eda392e6f251e4bba4fbe22d53154e1e7d9d4dba
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-11-21 12:30:16 -0300

    Agregada funcion que calcula el timeout con algoritmo de Karn.

--- client.py
+++ client.py
@@ -1,1 +1,16 @@
 # archivo de cliente
+
+SampleRTT = 0
+EstimatedRTT = 0
+DevRTT = 0
+timeout = 1
+alpha = 1/8
+beta = 1/4
+
+
+def karn_algorithm(sample):
+    global SampleRTT, EstimatedRTT, DevRTT, timeout
+    SampleRTT = sample
+    EstimatedRTT = (1-alpha)*EstimatedRTT + alpha*SampleRTT
+    DevRTT = (1-beta)*DevRTT + beta*abs(SampleRTT-EstimatedRTT)
+    timeout = EstimatedRTT + 4*DevRTT
